6d6f6fb598fd5791729c5ea1e8e46e36d7e5f14b
lukevukovich <lukevuke83@gmail.com>
2025-08-11 22:05:54 -0400

    Add sorting mechanic for favorite titles, add support for series

--- update.py
+++ update.py
@@ -1,11 +1,20 @@
-import json, os, requests
+import json, os, requests, sys
 
-SOURCES = {
-    "slasher": "https://mdblist.com/lists/slander2328/slasher-movies/json"
-}
+CONTENT_TYPE = sys.argv[1]
+if not CONTENT_TYPE:
+    print("Content type is required")
+    sys.exit(1)
 
-def fetch_and_save(catalog_id, url):
-    print(f"Updating {catalog_id} from {url}")
+SOURCES = json.loads(open(f"sources/{CONTENT_TYPE}_sources.json", "r", encoding="utf-8").read())
+
+def priority_index(title_lc: str, sort: list) -> int:
+    for i, term in enumerate(sort):
+        if term in title_lc:
+            return i
+    return len(sort) + 1 
+
+def fetch_and_save(catalog_id, url, sort):
+    print(f"Updating '{catalog_id}' from {url}")
     resp = requests.get(url)
     resp.raise_for_status()
     movies = resp.json()
@@ -15,16 +24,22 @@ def fetch_and_save(catalog_id, url):
             "id": m["imdb_id"],
             "type": m["mediatype"],
             "name": m["title"],
-            "year": m["release_year"]
+            "year": m["release_year"] if m.get("release_year") else 0
         }
         for m in movies if m.get("imdb_id")
     ]
+    if sort:
+            top_matches = [m for m in metas if any(term in m["name"].lower() for term in sort)]
+            others = [m for m in metas if m not in top_matches]
+            top_matches.sort(key=lambda x: (priority_index(x["name"].lower(), sort), -x["year"]))
+            others.sort(key=lambda x: (-x["year"], x["name"].lower()))
+            metas = top_matches + others
 
-    os.makedirs(f"catalogs/movie", exist_ok=True)
-    with open(f"catalogs/movie/{catalog_id}.json", "w", encoding="utf-8") as f:
+    os.makedirs(f"catalog/{CONTENT_TYPE}", exist_ok=True)
+    with open(f"catalog/{CONTENT_TYPE}/{catalog_id}.json", "w", encoding="utf-8") as f:
         json.dump({"metas": metas}, f, ensure_ascii=False, indent=2)
 
-for cid, src_url in SOURCES.items():
-    fetch_and_save(cid, src_url)
+for cid in SOURCES.keys():
+    fetch_and_save(cid, SOURCES[cid]["url"], SOURCES[cid]["sort"])
 
 print("All catalogs updated.")
